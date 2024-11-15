319180a62af59ed46df1381497b45d30f2aa78b1
Saad <norelyaqinesaad8@gmail.com>
2024-11-16 00:17:10 +0100

    Updating file hierarchy

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        WindowPanel wp = new WindowPanel();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Surveillance System");
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.add(wp);
+        window.setVisible(true);
+
+
+        window.pack();
+        wp.startThread();
+
+
+    }
+}
