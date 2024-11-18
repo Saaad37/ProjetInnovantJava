b280e4c2933fed150dfb9ce86257411966826362
Saad <norelyaqinesaad8@gmail.com>
2024-11-18 15:19:33 +0100

    Adding more realistic values picking

--- src/WindowPanel.java
+++ src/WindowPanel.java
@@ -1,24 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
-public class WindowPanel extends JPanel implements Runnable{
+public class WindowPanel extends JPanel implements Runnable {
 
     int pressureVal;
     int prcntN2Val;
     int prcntO2Val;
     int depthVal;
 
+    // TODO Comments and drawing graphs
+
     Thread windowThread;
     Random rand = new Random();
-    Font font = new Font("Helvetica", Font.PLAIN,20);
+    Font font = new Font("Helvetica", Font.PLAIN, 20);
     JLabel[] texts;
     Color bgColor = new Color(24, 124, 209);
     int[] values = new int[4];
     int[][] savedValues = new int[7200][4]; // [[], [], [], ...]
     int timerIterations;
 
-
+    ArrayList<Integer> pressureBound = new ArrayList<Integer>();
+    ArrayList<Integer> prcntN2Bound = new ArrayList<Integer>();
+    ArrayList<Integer> prcntO2Bound = new ArrayList<Integer>();
+    ArrayList<Integer> depthBound = new ArrayList<Integer>();
 
     JLabel pressureTxt = new JLabel();
     JLabel prcntN2Txt = new JLabel();
@@ -37,7 +43,7 @@ public class WindowPanel extends JPanel implements Runnable{
     long finalTime;
     double interval;
 
-    public WindowPanel(){
+    public WindowPanel() {
 
         pressureTxt.setFont(font);
         prcntN2Txt.setFont(font);
@@ -49,7 +55,7 @@ public class WindowPanel extends JPanel implements Runnable{
         prcntN2Txt.setBounds(new Rectangle(10, 150, 720, 50));
         prcntO2Txt.setBounds(new Rectangle(10, 200, 720, 50));
         depthTxt.setBounds(new Rectangle(10, 250, 720, 50));
-        savedText.setBounds(new Rectangle(50, 400, 720,50));
+        savedText.setBounds(new Rectangle(50, 400, 720, 50));
 
         this.setLayout(null);
         this.setPreferredSize(new Dimension(720, 600));
@@ -62,74 +68,81 @@ public class WindowPanel extends JPanel implements Runnable{
         this.add(savedText);
     }
 
-    public void startThread(){
+    public void startThread() {
         windowThread = new Thread(this);
         windowThread.start();
     }
 
-    public void assignValues(){
-        pressureVal = rand.nextInt(100);
-        prcntN2Val = rand.nextInt(100);
-        prcntO2Val = rand.nextInt(100);
-        depthVal = rand.nextInt(120);
-        
+    public void assignValues() {
+        if (timerIterations == 0) {
+            pressureVal = rand.nextInt(15, 100);
+            prcntN2Val = rand.nextInt(92);
+            prcntO2Val = rand.nextInt(80, 100);
+            depthVal = rand.nextInt(120);
+        } else {
+            pressureVal = rand.nextInt(Math.abs(pressureVal - 5), pressureVal + 5);
+            prcntN2Val = rand.nextInt(Math.abs(prcntN2Val - 5), prcntN2Val + 5);
+            prcntO2Val = rand.nextInt(Math.abs(prcntO2Val - 5), prcntO2Val + 5);
+            depthVal = rand.nextInt(Math.abs(depthVal - 5), depthVal + 5);
+
+        }
         values[0] = pressureVal;
         values[1] = prcntN2Val;
         values[2] = prcntO2Val;
         values[3] = depthVal;
-    }   
+    }
 
-
-    public void checkValues(){
-        if(pressureVal >= 50){
+    public void checkValues() {
+        if (pressureVal >= 50) {
             pressureWarning = "RETURN TO THE SURFACE PRESSURE TOO HIGH";
             pressureTxt.setForeground(Color.RED);
-        }else if(pressureVal < 50){
+        } else if (pressureVal < 50) {
             pressureWarning = "Normal pressure, just be careful..";
             pressureTxt.setForeground(Color.BLACK);
         }
-        if(prcntN2Val >= 10){
+        if (prcntN2Val >= 10) {
             N2Warning = "RETURN TO THE SURFACE N2 IN BLOOD IS TOO HIGH";
             prcntN2Txt.setForeground(Color.RED);
-        }else if(prcntN2Val < 10){
+        } else if (prcntN2Val < 10) {
             N2Warning = "Normal N2 percentages";
             prcntN2Txt.setForeground(Color.black);
         }
-        if(prcntO2Val >= 92){
+        if (prcntO2Val >= 92) {
             O2Warning = "Normal Saturation.";
             prcntO2Txt.setForeground(Color.black);
-        }else if(prcntO2Val < 92){
+        } else if (prcntO2Val < 92) {
             O2Warning = "O2 SATURATION IS TOO LOW !";
             prcntO2Txt.setForeground(Color.RED);
         }
-        if(depthVal >= 1 && depthVal < 58){
+        if (depthVal >= 1 && depthVal < 58) {
             depthWarning = "You are using a normal nitrox mixture.";
             depthTxt.setForeground(new Color(98, 209, 24));
-        }else if(depthVal > 58 && depthVal <= 60){
+        } else if (depthVal > 58 && depthVal <= 60) {
             depthWarning = "Be careful you are switching to a trimix gaz mixture";
             depthTxt.setForeground(Color.gray);
-        }else if(depthVal > 60 && depthVal < 120){
+        } else if (depthVal > 60 && depthVal < 120) {
             depthWarning = "Be careful you are not using a normal gaz mixture";
             depthTxt.setForeground(Color.RED);
         }
 
     }
 
-    public void saveValues(){
+    public void saveValues() {
         savedValues[timerIterations] = values;
     }
 
     @Override
     public void run() {
-        interval = 1000000000/fps;
+        interval = 1000000000 / fps;
         deltaT = 0;
         finalTime = System.nanoTime();
         long currentTime;
         timer = 0;
-        
-        while(windowThread != null){
-            
-            if(timerIterations < 0) timerIterations = 0;
+
+        while (windowThread != null) {
+
+            if (timerIterations < 0)
+                timerIterations = 0;
 
             currentTime = System.nanoTime();
 
@@ -137,13 +150,11 @@ public class WindowPanel extends JPanel implements Runnable{
             timer += currentTime - finalTime;
             finalTime = currentTime;
 
-
-            if(deltaT >= 1)
-            {
+            if (deltaT >= 1) {
                 deltaT--;
                 timer++;
             }
-            if(timer >= 1000000000){
+            if (timer >= 1000000000) {
                 showSavedValues();
                 assignValues();
                 checkValues();
@@ -158,21 +169,20 @@ public class WindowPanel extends JPanel implements Runnable{
 
     public void displayValues() {
 
-        pressureTxt.setText("<html><body><p>Pressure :" + pressureVal + " bar " + pressureWarning + " </p><br> </body></html>");
+        pressureTxt.setText(
+                "<html><body><p>Pressure :" + pressureVal + " bar " + pressureWarning + " </p><br> </body></html>");
         prcntN2Txt.setText("<html><body><p>N2 :" + prcntN2Val + "% " + N2Warning + "</p><br></body></html>");
         prcntO2Txt.setText("<html><body><p>O2 :" + prcntO2Val + "% " + O2Warning + "</p><br></body></html>");
         depthTxt.setText("<html><body><p>Depth :" + depthVal + "m " + depthWarning + "</p><br></body></html>");
 
-        
     }
 
-
-    public void showSavedValues(){
-        if(timerIterations >= 1){
+    public void showSavedValues() {
+        if (timerIterations >= 1) {
             savedText.setText("Pressure: " + savedValues[timerIterations - 1][0]
-            + " bar N2 : " + savedValues[timerIterations - 1][1] 
-            + "% O2: " + savedValues[timerIterations - 1][2]
-            + "% Depth: " + savedValues[timerIterations - 1][3] + " m");
+                    + " bar N2 : " + savedValues[timerIterations - 1][1]
+                    + "% O2: " + savedValues[timerIterations - 1][2]
+                    + "% Depth: " + savedValues[timerIterations - 1][3] + " m");
         }
     }
 }
