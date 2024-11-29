c5cabecb138deca917e22bbc240b267cc1f7c2cd
Saad <norelyaqinesaad8@gmail.com>
2024-11-29 22:59:56 +0100

    adding panel in the main class and not in the window Comp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        WindowPanel wp = new WindowPanel();
         Window window = new Window();
+        window.add(wp); // Ajouter le panel à la fenêtre.
         window.pack(); // Affichage de la fenêtre.
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,21 +1,14 @@
 import javax.swing.*;
 
-public class Window extends JFrame{
+public class Window extends JFrame {
 
-    WindowPanel wp;
-
-    public Window(){
-        wp = new WindowPanel();
+    public Window() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Surveillance system");
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-        this.add(wp); // Ajouter le panel à la fenêtre.
         this.setVisible(true);
 
-
     }
-
-
 
 }
