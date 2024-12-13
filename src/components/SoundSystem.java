7575c9f3dcab518b9451fa9821dff269991c1b34
Saad <norelyaqinesaad8@gmail.com>
2024-12-13 11:16:37 +0100

    Adding a parameter to the playSound to loop or not

--- src/components/SoundSystem.java
+++ src/components/SoundSystem.java
@@ -1,6 +1,7 @@
 package components;
 
 import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 
 public class SoundSystem {
@@ -10,21 +11,22 @@ public class SoundSystem {
 
     public SoundSystem(String path) {
         try {
-            this.AIS = AudioSystem.getAudioInputStream(this.getClass().getResourceAsStream(path));
+            this.AIS = AudioSystem.getAudioInputStream(new BufferedInputStream(this.getClass().getResourceAsStream(path
+            )));
             this.audio = AudioSystem.getClip();
         } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void playSound() {
+    public void playSound(int numLoops) {
         try {
             if (!this.audio.isOpen())
                 audio.open(this.AIS);
         } catch (LineUnavailableException | IOException e) {
             e.printStackTrace();
         }
-        audio.loop(1);
+        audio.loop(numLoops);
     }
 
     public void stopSound() {
