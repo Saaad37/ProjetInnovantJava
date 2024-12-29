9e9763c04c85fbec66b4d99c8e292a5da4f5ab3f
Saad <norelyaqinesaad8@gmail.com>
2024-12-29 01:12:12 +0100

    checks if files exists if not it will create it //Linux

--- src/components/FileIO.java
+++ src/components/FileIO.java
@@ -1,22 +1,24 @@
 package components;
 
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileIO {
 
     BufferedReader br;
     public FileIO(String filePath){
-
         try {
             br = new BufferedReader(new FileReader(filePath));
+            System.out.println("File Exists.");
         } catch (FileNotFoundException e) {
-            ErrorDialogBox err = new ErrorDialogBox("Could not locate text file");
-            err.setVisible(true);
+            File file = new File(filePath);
+            try {
+                if(file.createNewFile()){
+                    System.out.println("File Created " + file.getName());
+                }
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
-
     }
 }
