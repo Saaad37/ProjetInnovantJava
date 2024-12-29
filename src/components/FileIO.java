de02aa003f1cf02c1421d31a947161ab15ee1277
Saad <norelyaqinesaad8@gmail.com>
2024-12-29 01:01:10 +0100

    Adding the basic try catch of reading file

--- src/components/FileIO.java
+++ src/components/FileIO.java
@@ -1,4 +1,22 @@
 package components;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileIO {
+
+    BufferedReader br;
+    public FileIO(String filePath){
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+            ErrorDialogBox err = new ErrorDialogBox("Could not locate text file");
+            err.setVisible(true);
+        }
+
+    }
 }
