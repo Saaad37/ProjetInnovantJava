d60099095ecbf43be05652765061f649a7f519c8
Saad <norelyaqinesaad8@gmail.com>
2025-01-09 23:32:55 +0100

    Adding PasswordField and userIcon

--- src/components/EnterCredsBox.java
+++ src/components/EnterCredsBox.java
@@ -1,9 +1,15 @@
 package components;
 
+import main.WindowPanel;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class EnterCredsBox extends JFrame {
+
+    JTextField username;
+    JPasswordField password;
+    ImageIcon user = new ImageIcon(this.getClass().getResource("/assets/user.png"));
 
     public EnterCredsBox(){
         this.setLayout(null);
@@ -12,29 +18,38 @@ public class EnterCredsBox extends JFrame {
         this.setTitle("Enter Credentials");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        Dimension entryTextDim = new Dimension(120, 35);
+        user.setImage(WindowPanel.resizeImage(user, 25, 25).getImage());
+        JLabel userL = new JLabel(user);
+        userL.setBounds(85, 125, 25, 25);
 
         JLabel l = new JLabel("Enter your MySQL username and password", SwingConstants.CENTER);
         l.setBounds(55, 25, 600, 50);
         l.setFont(new Font("Helvetica", Font.PLAIN, 26));
 
-        JTextField username = new JTextField();
+        username = new JTextField();
+        username.setFont(new Font("Arial", Font.PLAIN, 20));
+        username.setBounds(120, 120, 500, 35);
 
+        password = new JPasswordField();
+        password.setFont(new Font("Arial", Font.PLAIN, 20));
+        password.setBounds(120, 200, 500, 35);
 
 
         this.add(l);
+        this.add(userL);
         this.add(username);
+        this.add(password);
 
         this.pack();
     }
 
 
     public String getUsername(){
-        return "";
+        return username.getText();
     }
 
-    public String getPassword(){
-        return "";
+    public char[] getPassword(){
+        return password.getPassword();
     }
 
     public static void main(String[] args) {
