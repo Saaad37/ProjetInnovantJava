e6f306f7c6b3f3b9d2c6bf23b616a2dbaceb522a
Saad <norelyaqinesaad8@gmail.com>
2025-01-09 23:14:46 +0100

    Setting layout to null and adding bounds

--- src/components/EnterCredsBox.java
+++ src/components/EnterCredsBox.java
@@ -6,16 +6,19 @@ import java.awt.*;
 public class EnterCredsBox extends JFrame {
 
     public EnterCredsBox(){
-        this.setLayout(new GridLayout(4, 1, 20, 20));
+        this.setLayout(null);
         this.setPreferredSize(new Dimension(720, 480));
         this.setLocationRelativeTo(null);
         this.setTitle("Enter Credentials");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        Dimension entryTextDim = new Dimension(120, 35);
+
         JLabel l = new JLabel("Enter your MySQL username and password", SwingConstants.CENTER);
+        l.setBounds(55, 25, 600, 50);
         l.setFont(new Font("Helvetica", Font.PLAIN, 26));
 
         JTextField username = new JTextField();
-        username.setPreferredSize(new Dimension(120, 30));
 
 
 
