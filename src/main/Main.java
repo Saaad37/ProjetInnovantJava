403c9810ce1364a975775009f54c84875e5eb332
Saad <norelyaqinesaad8@gmail.com>
2025-01-09 22:27:05 +0100

    Adding the condition for linux os

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,9 +8,28 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
         WindowPanel wp = new WindowPanel();
         Window window = new Window("Surveillance System");
+
+        String name = System.getProperty("user.name");
+        if(FileIO.getOS().equalsIgnoreCase("Linux")){
+            String dirPath = "/home/" + name + "/SurveillanceSystem";
+            File dir = new File(dirPath);
+            if(!dir.exists()){
+                if(dir.mkdir()){
+                    System.out.println("Directory created successfully.");
+                }else{
+                    ErrorDialogBox e = new ErrorDialogBox("Cannot created the file");
+                    e.setVisible(true);
+                    try {
+                        e.wait(2500);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    System.exit(0);
+                }
+            }
+        }
 
         window.add(wp);
 
