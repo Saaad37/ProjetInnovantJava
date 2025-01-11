c78293418bd1718b02e5089af10eff28316f201f
Saad <norelyaqinesaad8@gmail.com>
2025-01-11 12:29:29 +0100

    Adding some error Handling //TODO

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,21 +10,29 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         String name = System.getProperty("user.name");
-        if(FileIO.getOS().equalsIgnoreCase("Linux")){
+        if (FileIO.getOS().equalsIgnoreCase("Linux")) {
             String dirPath = "/home/" + name + "/SurveillanceSystem";
             File dir = new File(dirPath);
-            if(!dir.exists()){
-                if(dir.mkdir()){
-                    System.out.println("Directory created successfully.");
-                    EnterCredsBox ecb = new EnterCredsBox();
-                    ecb.setVisible(true);
+            if (!dir.exists()) {
+                if (dir.mkdir()) {
                     FileIO creds = new FileIO(dirPath + "/creds.txt");
-                    while(ecb.isbClicked()){
-                        creds.writeCreds(ecb);
-                    }
-
-                }else{
-                    ErrorDialogBox e = new ErrorDialogBox("Cannot created the directory");
+                    WriteAndSave(creds);
+                }
+            } else {
+                FileIO creds = new FileIO(dirPath + "/creds.txt");
+                if (!creds.getFile().exists()) {
+                    WriteAndSave(creds);
+                }
+            }
+        } else if (FileIO.getOS().equalsIgnoreCase("Windows 10") ||
+                FileIO.getOS().equalsIgnoreCase("Windows 11")) {
+            String dirPath = "C:\\Users\\" + name + "\\SurveillanceSystem";
+            File dir = new File(dirPath);
+            if (!dir.exists()) {
+                if (dir.mkdir()) {
+                    System.out.println("Directory created successfully.");
+                } else {
+                    ErrorDialogBox e = new ErrorDialogBox("Cannot create the directory");
                     e.setVisible(true);
                     try {
                         e.wait(2500);
@@ -34,30 +42,31 @@ public class Main {
                     System.exit(0);
                 }
             }
-        }else if(FileIO.getOS().equalsIgnoreCase("Windows 10") ||
-                FileIO.getOS().equalsIgnoreCase("Windows 11")){
-                String dirPath = "C:\\Users\\" + name + "\\SurveillanceSystem";
-                File dir = new File(dirPath);
-                if(!dir.exists()){
-                    if(dir.mkdir()){
-                        System.out.println("Directory created successfully.");
-                    }else{
-                        ErrorDialogBox e = new ErrorDialogBox("Cannot create the directory");
-                        e.setVisible(true);
-                        try {
-                            e.wait(2500);
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-                        System.exit(0);
-                    }
-                }
         }
         // TODO Mac OS X condition.
 
     }
 
-    private void startProg(){
+    private static void WriteAndSave(FileIO creds) {
+        if (creds.getFile().exists()) {
+            System.out.println("Directory created successfully.");
+            EnterCredsBox ecb = new EnterCredsBox(creds);
+            ecb.setVisible(true);
+
+            startProg();
+        } else {
+            ErrorDialogBox e = new ErrorDialogBox("Cannot created the directory");
+            e.setVisible(true);
+            try {
+                e.wait(2500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.exit(0);
+        }
+    }
+
+    private static void startProg(){
         WindowPanel wp = new WindowPanel();
         Window window = new Window("Surveillance System");
 
